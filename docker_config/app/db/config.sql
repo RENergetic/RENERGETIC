a5016f3e68ce25ff9cd768be96fb4559335054af
Raul Parra Ruiz <raul.parra.ext@inetum.com>
2022-04-18 15:32:30 +0200

    WSO2 data persistence

--- docker_config/app/db/config.sql
+++ docker_config/app/db/config.sql
@@ -1,3 +1,11 @@
-CREATE DATABASE keycloak
+SELECT 
+   'CREATE DATABASE keycloak
+      WITH OWNER postgres 
+      CONNECTION LIMIT  -1'
+   WHERE NOT EXISTS (SELECT FROM pg_database WHERE datname = 'keycloak')\gexec
+
+SELECT 
+'CREATE DATABASE wso
    WITH OWNER postgres 
-   CONNECTION LIMIT  -1;
+   CONNECTION LIMIT  -1'
+WHERE NOT EXISTS (SELECT FROM pg_database WHERE datname = 'wso')\gexec
