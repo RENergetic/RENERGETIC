4eefa1e3c6ca5c26126a2836fc05fe9f1e5bfa09
Raul Parra Ruiz <raul.parra.ext@inetum.com>
2022-04-28 10:16:12 +0200

    docker_config_local update

--- docker_config_local/data/installerWindows.sh
+++ docker_config_local/data/installerWindows.sh
@@ -7,7 +7,7 @@ installapi='true'
 installback='false'
 installkafka='false'
 installgrafana='true'
-javafile='backinflux-0.0.1-SNAPSHOT.jar'
+javafile='measurementapi-0.0.1-SNAPSHOT.jar'
 
 while getopts n: flag
 do
