352cfc66962a8f23177c8e7939578f17cc71e2b4
Raul Parra Ruiz <raul.parra.ext@inetum.com>
2022-08-09 12:40:42 +0200

    Added configuration files to Nifi and relational database

---
+++ configuration/relational_db/APIs/README.md
@@ -0,0 +1,43 @@
+# INTRODUCTION
+Here you can find scripts to insert data in relational database using HDR API
+
+This scripts allow to create:
+ - **AssetTypes**: you can find the list of AssetTypes to create in [AssetTypes file](src/types/assetTypes.js)
+ - **MeasurementTypes**: you can find the list of MeasurementTypes to create in [MeasurementTypes file](src/types/measurementTypes.js)
+ - **Scenarios**: the elements that creates each scenario can be found in the schemas at [Scenarios folder](scenarios/)
+
+To configure the API URL and other paramenters you can modify the [.env file](.env)
+
+## HOW TO USE
+
+*To use it you should have installed Node in your computer.* [Install it here](https://nodejs.org/en/download/)
+
+The command to run the scripts is `npm generate`
+
+It have some parameters:
+ - --scenario: to set the scenario to create, if the scenario isn't exists show a message, if you call the command without this parameter no scenario is created
+ - --assetTypes: setting it to *false* the asset types isn't created
+ - --measurementTypes: setting it to *false* the measurement types isn't created
+ - --influxRequirements: setting it to *true* create allowed measurements and tags (Is recommended generate measurement types also using measurementTypes argument)
+
+The parameters should be write after *' -- '* and to set a value to it use *'='* operator
+
+### Examples
+
+Set you CLI at this folder and:
+
+To generate only the scenario (*WARN | The scenario can need that the types are in the database*)
+
+`npm generate -- --scenario=1`
+
+If you only want generate the types:
+
+`npm generate -- --assetTypes --measurementTypes`
+
+To generate InfluxDB requirements (measurements and tags)
+
+`npm generate -- --influxRequirements`
+
+To generate all
+
+`npm generate -- --scenario=1 --assetTypes --measurementTypes --influxRequirements`
---
+++ configuration/relational_db/APIs/src/types/assetTypes.js
@@ -0,0 +1,144 @@
+
+import { post } from "../utils.js";
+const path = "api/assets/type"
+
+const types = [
+    {
+        id: "1",
+        name: "room",
+        label: "Room",
+        category: "structural",
+        renovable: null
+    },
+    {
+        id: "2",
+        name: "flat",
+        label: "Flat",
+        category: "structural",
+        renovable: null
+    },
+    {
+        id: "3",
+        name: "building",
+        label: "Building",
+        category: "structural",
+        renovable: null
+    },
+    {
+        id: "4",
+        name: "energy_island",
+        label: "Energy island",
+        category: "structural",
+        renovable: null
+    },
+    {
+        id: "5",
+        name: "generation_plant",
+        label: "Generation plant",
+        category: "energy",
+        renovable: 50
+    },
+    {
+        id: "6",
+        name: "heat_pump",
+        label: "Heat pump",
+        category: "energy",
+        renovable: 40
+    },
+    {
+        id: "7",
+        name: "gas_boiler",
+        label: "Gas boiler",
+        category: "energy",
+        renovable: 0
+    }, 
+    {
+        id: "8",
+        name: "co-generation_unit",
+        label: "Co-generation unit",
+        category: "energy",
+        renovable: 0
+    }, 
+    {
+        id: "9",
+        name: "coal_plant",
+        label: "Coal plant",
+        category: "energy",
+        renovable: 0
+    }, 
+    {
+        id: "10",
+        name: "pv_plant",
+        label: "PV plant",
+        category: "energy",
+        renovable: 100
+    }, 
+    {
+        id: "11",
+        name: "external_heat_grid",
+        label: "External heat grid",
+        category: "energy",
+        renovable: null
+    }, 
+    {
+        id: "12",
+        name: "external_electricity_grid",
+        label: "External electricity grid",
+        category: "energy",
+        renovable: null
+    }, 
+    {
+        id: "13",
+        name: "solar_thermal_collector",
+        label: "Solar thermal collectro",
+        category: "energy",
+        renovable: 100
+    }, 
+    {
+        id: "14",
+        name: "steam",
+        label: "Steam",
+        category: "infrastructure",
+        renovable: null
+    }, 
+    {
+        id: "15",
+        name: "district_heating",
+        label: "District heating",
+        category: "infrastructure",
+        renovable: null
+    }, 
+    {
+        id: "16",
+        name: "district_cooling",
+        label: "District cooling",
+        category: "infrastructure",
+        renovable: null
+    }, 
+    {
+        id: "17",
+        name: "electricity",
+        label: "Electricity",
+        category: "infrastructure",
+        renovable: null
+    }, 
+    {
+        id: "18",
+        name: "user",
+        label: "User",
+        category: "infrastructure",
+        renovable: null
+    },
+    {
+        id: "19",
+        name: "ev_charging_station",
+        label: "Ev charging station",
+        category: "energy",
+        renovable: 100
+    },
+];
+
+export async function generateAssetTypes() {
+    for (let type of types)
+        await post(path, type);
+}
