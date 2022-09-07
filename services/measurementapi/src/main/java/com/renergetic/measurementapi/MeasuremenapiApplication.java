6fdbdb3b3a0c32e35d251063bb6199ea2a1d729a
Raul Parra Ruiz <raul.parra.ext@inetum.com>
2022-09-07 12:03:53 +0200

    Added docs to measurement API

--- services/measurementapi/src/main/java/com/renergetic/measurementapi/MeasuremenapiApplication.java
+++ services/measurementapi/src/main/java/com/renergetic/measurementapi/MeasuremenapiApplication.java
@@ -3,7 +3,11 @@ package com.renergetic.measurementapi;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
+
 @SpringBootApplication
+@OpenAPIDefinition(info = @Info(title = "Measurement API", version = "1.0", description = "API to insert and retrieve time-series InfluxDB data"))
 public class MeasuremenapiApplication {
 
 	public static void main(String[] args) {
