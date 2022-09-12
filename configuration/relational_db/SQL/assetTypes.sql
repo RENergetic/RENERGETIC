f27ec2990a7e7141d84236086ccb0f29fd06046c
Raul Parra Ruiz <raul.parra.ext@inetum.com>
2022-09-12 12:13:35 +0200

    Checked and fixed SQL files to create data at PostgreSQL

--- configuration/relational_db/SQL/assetTypes.sql
+++ configuration/relational_db/SQL/assetTypes.sql
@@ -1,41 +1,41 @@
 
 INSERT INTO asset_type ( id, name, label, category, renovable ) 
-    VALUES( 1, "room", "Room", "structural", NULL );
+    VALUES( 1, 'room', 'Room', 'structural', NULL );
 INSERT INTO asset_type ( id, name, label, category, renovable ) 
-    VALUES( 2, "flat", "Flat", "structural", NULL );
+    VALUES( 2, 'flat', 'Flat', 'structural', NULL );
 INSERT INTO asset_type ( id, name, label, category, renovable ) 
-    VALUES( 3, "building", "Building", "structural", NULL );
+    VALUES( 3, 'building', 'Building', 'structural', NULL );
 INSERT INTO asset_type ( id, name, label, category, renovable ) 
-    VALUES( 4, "energy_island", "Energy island", "structural", NULL );
+    VALUES( 4, 'energy_island', 'Energy island', 'structural', NULL );
 INSERT INTO asset_type ( id, name, label, category, renovable ) 
-    VALUES( 5, "generation_plant", "Generation plant", "energy", 50 );
+    VALUES( 5, 'generation_plant', 'Generation plant', 'energy', 50 );
 INSERT INTO asset_type ( id, name, label, category, renovable ) 
-    VALUES( 6, "heat_pump", "Heat pump", "energy", 40 );
+    VALUES( 6, 'heat_pump', 'Heat pump', 'energy', 40 );
 INSERT INTO asset_type ( id, name, label, category, renovable ) 
-    VALUES( 7, "gas_boiler", "Gas boiler", "energy", 0 );
+    VALUES( 7, 'gas_boiler', 'Gas boiler', 'energy', 0 );
 INSERT INTO asset_type ( id, name, label, category, renovable ) 
-    VALUES( 8, "co-generation_unit", "Co-generation unit", "energy", 0 );
+    VALUES( 8, 'co-generation_unit', 'Co-generation unit', 'energy', 0 );
 INSERT INTO asset_type ( id, name, label, category, renovable ) 
-    VALUES( 9, "coal_plant", "Coal plant", "energy", 0 );
+    VALUES( 9, 'coal_plant', 'Coal plant', 'energy', 0 );
 INSERT INTO asset_type ( id, name, label, category, renovable ) 
-    VALUES( 10, "pv_plant", "PV plant", "energy", 100 );
+    VALUES( 10, 'pv_plant', 'PV plant', 'energy', 100 );
 INSERT INTO asset_type ( id, name, label, category, renovable ) 
-    VALUES( 11, "external_heat_grid", "External heat grid", "energy", NULL );
+    VALUES( 11, 'external_heat_grid', 'External heat grid', 'energy', NULL );
 INSERT INTO asset_type ( id, name, label, category, renovable ) 
-    VALUES( 12, "external_electricity_grid", "External electricity grid", "energy", NULL );
+    VALUES( 12, 'external_electricity_grid', 'External electricity grid', 'energy', NULL );
 INSERT INTO asset_type ( id, name, label, category, renovable ) 
-    VALUES( 13, "solar_thermal_collector", "Solar thermal collector", "energy", 100 );
+    VALUES( 13, 'solar_thermal_collector', 'Solar thermal collector', 'energy', 100 );
 INSERT INTO asset_type ( id, name, label, category, renovable ) 
-    VALUES( 14, "steam", "Steam", "infrastructure", NULL );
+    VALUES( 14, 'steam', 'Steam', 'infrastructure', NULL );
 INSERT INTO asset_type ( id, name, label, category, renovable ) 
-    VALUES( 15, "district_heating", "District heating", "infrastructure", NULL );
+    VALUES( 15, 'district_heating', 'District heating', 'infrastructure', NULL );
 INSERT INTO asset_type ( id, name, label, category, renovable ) 
-    VALUES( 16, "district_cooling", "District cooling", "infrastructure", NULL );
+    VALUES( 16, 'district_cooling', 'District cooling', 'infrastructure', NULL );
 INSERT INTO asset_type ( id, name, label, category, renovable ) 
-    VALUES( 17, "electricity", "Electricity", "infrastructure", NULL );
+    VALUES( 17, 'electricity', 'Electricity', 'infrastructure', NULL );
 INSERT INTO asset_type ( id, name, label, category, renovable ) 
-    VALUES( 18, "user", "User", "user", NULL );
+    VALUES( 18, 'user', 'User', 'user', NULL );
 INSERT INTO asset_type ( id, name, label, category, renovable ) 
-    VALUES( 19, "ev_charging_station", "Ev charging station", "energy", 100 );
+    VALUES( 19, 'ev_charging_station', 'Ev charging station', 'energy', 100 );
 
 COMMIT;
--- configuration/relational_db/SQL/scenario1.sql
+++ configuration/relational_db/SQL/scenario1.sql
@@ -1,78 +1,81 @@
 
-INSERT INTO uuid VALUES 2
+INSERT INTO uuid VALUES ('2');
 
 /* CREATE USERS */
-INSERT INTO users ( id, name, uuid ) 
-    VALUES( 1, "user1", "2" );
-INSERT INTO users ( id, name, uuid ) 
-    VALUES( 2, "user2", "2" );
+INSERT INTO users ( id, uuid ) 
+    VALUES( 1, '2' );
+INSERT INTO users ( id, uuid ) 
+    VALUES( 2, '2' );
 
 INSERT INTO asset ( id, name, label, asset_type_id, user_id, uuid ) 
-    VALUES( 1, "user1", "User 1", 18, 1, "2" );
+    VALUES( 1, 'user1', 'User 1', 18, 1, '2' );
 INSERT INTO asset ( id, name, label, asset_type_id, user_id, uuid ) 
-    VALUES( 2, "user2", "User 2", 18, 2, "2" );
+    VALUES( 2, 'user2', 'User 2', 18, 2, '2' );
 
 /* CREATE ISLAND */
 INSERT INTO asset ( id, name, label, asset_type_id, uuid ) 
-    VALUES( 3, "energy_island", "Energy Island", 4, "2" );
+    VALUES( 3, 'energy_island', 'Energy Island', 4, '2' );
 
 /* CREATE BUILDINGS */
 INSERT INTO asset ( id, name, label, asset_type_id, parent_asset_id, uuid ) 
-    VALUES( 4, "building2", "Building 2", 3, 3, "2" );
+    VALUES( 4, 'building2', 'Building 2', 3, 3, '2' );
 INSERT INTO asset ( id, name, label, asset_type_id, parent_asset_id, uuid ) 
-    VALUES( 5, "flat1", "Flat 1", 2, 4, "2" );
+    VALUES( 5, 'flat1', 'Flat 1', 2, 4, '2' );
 INSERT INTO asset ( id, name, label, asset_type_id, parent_asset_id, uuid ) 
-    VALUES( 6, "building1", "Building 1", 3, 3, "2" );
+    VALUES( 6, 'building1', 'Building 1', 3, 3, '2' );
 
 /* CREATE ENERGY ASSETS */
 INSERT INTO asset ( id, name, label, asset_type_id, uuid ) 
-    VALUES( 7, "gas_boiler1", "Gas Boiler 1", 7, "2" );
+    VALUES( 7, 'gas_boiler1', 'Gas Boiler 1', 7, '2' );
 INSERT INTO asset ( id, name, label, asset_type_id, uuid ) 
-    VALUES( 8, "gas_boiler2", "Gas Boiler 2", 7, "2" );
+    VALUES( 8, 'gas_boiler2', 'Gas Boiler 2', 7, '2' );
 INSERT INTO asset ( id, name, label, asset_type_id, uuid ) 
-    VALUES( 9, "solar_collector1", "Solar Collector 1", 13, "2" );
+    VALUES( 9, 'solar_collector1', 'Solar Collector 1', 13, '2' );
 
 /* CONNECT ASSETS */
-INSERT INTO asset_panel ( id, asset_id, connected_asset_id, connection_type )
-    VALUES( 1, 1, 5, "owner" )
-INSERT INTO asset_panel ( id, asset_id, connected_asset_id, connection_type )
-    VALUES( 1, 2, 5, "resident" )
-INSERT INTO asset_panel ( id, asset_id, connected_asset_id, connection_type )
-    VALUES( 1, 2, 6, "owner" )
+-- Conecction_type:
+--   0 owner
+--   1 resident
+INSERT INTO asset_connection ( id, asset_id, connected_asset_id, connection_type )
+    VALUES( 1, 1, 5, 0 );
+INSERT INTO asset_connection ( id, asset_id, connected_asset_id, connection_type )
+    VALUES( 2, 2, 5, 1 );
+INSERT INTO asset_connection ( id, asset_id, connected_asset_id, connection_type )
+    VALUES( 3, 2, 6, 0 );
     
-INSERT INTO asset_panel ( id, asset_id, connected_asset_id, connection_type )
-    VALUES( 1, 5, 7, NULL )
-INSERT INTO asset_panel ( id, asset_id, connected_asset_id, connection_type )
-    VALUES( 1, 6, 8, NULL )
-INSERT INTO asset_panel ( id, asset_id, connected_asset_id, connection_type )
-    VALUES( 1, 6, 9, NULL )
+INSERT INTO asset_connection ( id, asset_id, connected_asset_id, connection_type )
+    VALUES( 4, 5, 7, NULL );
+INSERT INTO asset_connection ( id, asset_id, connected_asset_id, connection_type )
+    VALUES( 5, 6, 8, NULL );
+INSERT INTO asset_connection ( id, asset_id, connected_asset_id, connection_type )
+    VALUES( 6, 6, 9, NULL );
 
 /* CREATE MEASUREMENTS */
 INSERT INTO measurement ( id, name, label, measurement_type_id, domain, direction, sensor_name, asset_id, uuid ) 
-    VALUES( 1, "renewability", "Renewability", 5, "heat", NULL, "renewability", 5, "2" );
+    VALUES( 21, 'renewability', 'Renewability', 5, 'heat', NULL, 'renewability', 5, '2' );
 INSERT INTO measurement ( id, name, label, measurement_type_id, domain, direction, sensor_name, asset_id, uuid ) 
-    VALUES( 2, "renewability", "Renewability", 5, "heat", NULL, "renewability", 6, "2" );
+    VALUES( 22, 'renewability', 'Renewability', 5, 'heat', NULL, 'renewability', 6, '2' );
 
 INSERT INTO measurement ( id, name, label, measurement_type_id, domain, direction, sensor_name, asset_id, uuid ) 
-    VALUES( 3, "heat_consumed", "Heat Consumed", 7, "heat", "in", "thermostat", 5, "2" );
+    VALUES( 23, 'heat_consumed', 'Heat Consumed', 7, 'heat', 'in', 'thermostat', 5, '2' );
 INSERT INTO measurement ( id, name, label, measurement_type_id, domain, direction, sensor_name, asset_id, uuid ) 
-    VALUES( 4, "heat_consumed", "Heat Consumed", 7, "heat", "in", "thermostat", 6, "2" );
+    VALUES( 24, 'heat_consumed', 'Heat Consumed', 7, 'heat', 'in', 'thermostat', 6, '2' );
 INSERT INTO measurement ( id, name, label, measurement_type_id, domain, direction, sensor_name, asset_id, uuid ) 
-    VALUES( 5, "heat_consumed", "Heat Consumed", 7, "heat", "in", "thermostat", 4, "2" );
+    VALUES( 25, 'heat_consumed', 'Heat Consumed', 7, 'heat', 'in', 'thermostat', 4, '2' );
 
 INSERT INTO measurement ( id, name, label, measurement_type_id, domain, direction, sensor_name, asset_id, uuid ) 
-    VALUES( 6, "heat_supply", "Heat Supply", 7, "heat", "out", "gas_boiler", 7, "2" );
+    VALUES( 26, 'heat_supply', 'Heat Supply', 7, 'heat', 'out', 'gas_boiler', 7, '2' );
 INSERT INTO measurement ( id, name, label, measurement_type_id, domain, direction, sensor_name, asset_id, uuid ) 
-    VALUES( 7, "heat_supply", "Heat Supply", 7, "heat", "out", "gas_boiler", 8, "2" );
+    VALUES( 27, 'heat_supply', 'Heat Supply', 7, 'heat', 'out', 'gas_boiler', 8, '2' );
 INSERT INTO measurement ( id, name, label, measurement_type_id, domain, direction, sensor_name, asset_id, uuid ) 
-    VALUES( 8, "heat_supply", "Heat Supply", 7, "heat", "out", "solar_collector", 9, "2" );
+    VALUES( 28, 'heat_supply', 'Heat Supply', 7, 'heat', 'out', 'solar_collector', 9, '2' );
 
 /* TILES AND PANLES */
 INSERT INTO information_panel ( id, name, label, uuid ) 
-    VALUES( 1, "panel1", "Panel 1", "2" );
+    VALUES( 1, 'panel1', 'Panel 1', '2' );
 
 INSERT INTO information_tile ( id, name, label, type, layout, information_panel_id ) 
-    VALUES( 1, "renewability", "Renewability", "knob", NULL, 1 );
+    VALUES( 1, 'renewability', 'Renewability', 'knob', NULL, 1 );
 
 INSERT INTO information_tile_measurement ( id, measurement_id, domain, direction, measurement_type_id, sensor_name, information_tile_id ) 
     VALUES( 1, 1, NULL, NULL, NULL, NULL, 1 );
@@ -81,22 +84,22 @@ INSERT INTO information_tile_measurement ( id, measurement_id, domain, direction
 
 /* CONNECT ASSETS WITH PANELS */
 INSERT INTO asset_panel ( panel_id, asset_id )
-    VALUES( 1, 5 )
+    VALUES( 1, 5 );
 INSERT INTO asset_panel ( panel_id, asset_id )
-    VALUES( 1, 6 )
+    VALUES( 1, 6 );
 
 /* USER DEMANDS DEFINITIONS */
 INSERT INTO user_demand_definition ( id, action, message, action_type ) 
-    VALUES( 1, "INCREASE_TEMPERATURE", "Please, increase the temperature", "INCREASE" );
+    VALUES( 1, 'INCREASE_TEMPERATURE', 'Please, increase the temperature', 'INCREASE' );
 INSERT INTO user_demand_definition ( id, action, message, action_type ) 
-    VALUES( 2, "DECREASE_TEMPERATURE", "Please, decrease the temperature", "DECREASE" );
+    VALUES( 2, 'DECREASE_TEMPERATURE', 'Please, decrease the temperature', 'DECREASE' );
 
 /* USER DEMAND SCHEDULES */
-INSERT INTO user_demand_schedule ( id, asset_id, demand_id, demand_start, demand_stop, demand_update ) 
-    VALUES( 1, 5, 1, now(), now() + interval "1 hour", now() );
-INSERT INTO user_demand_schedule ( id, asset_id, demand_id, demand_start, demand_stop, demand_update ) 
-    VALUES( 1, 6, 2, now(), now() + interval "1 hour", now() );
-INSERT INTO user_demand_schedule ( id, asset_id, demand_id, demand_start, demand_stop, demand_update ) 
-    VALUES( 1, 6, 2, now() - interval "2 hour", now() - interval "1 hour", now() );
+INSERT INTO user_demand_schedule ( id, asset_id, demand_id, demand_start, demand_stop, update_date ) 
+    VALUES( 1, 5, 1, now(), now() + interval '1 hour', now() );
+INSERT INTO user_demand_schedule ( id, asset_id, demand_id, demand_start, demand_stop, update_date ) 
+    VALUES( 2, 6, 2, now(), now() + interval '1 hour', now() );
+INSERT INTO user_demand_schedule ( id, asset_id, demand_id, demand_start, demand_stop, update_date ) 
+    VALUES( 3, 6, 2, now() - interval '2 hour', now() - interval '1 hour', now() );
 
 COMMIT;
