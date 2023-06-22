6092bcb5b8b301af2d0ad38199050930670e2f8d
Raul Parra Ruiz <raul.parra.ext@inetum.com>
2023-06-22 16:14:25 +0200

    Update date format to date converter

--- services/kpiAPI/src/main/java/com/renergetic/kpiapi/service/utils/DateConverter.java
+++ services/kpiAPI/src/main/java/com/renergetic/kpiapi/service/utils/DateConverter.java
@@ -11,7 +11,7 @@ import com.renergetic.kpiapi.exception.InvalidArgumentException;
 
 public final class DateConverter {
     //TODO: Raul set date format as you wish :)
-    private static SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd hh:mm:ss");
+    private static SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
 
     private static ZoneId getTimezone() {
         return ZoneId.systemDefault();
