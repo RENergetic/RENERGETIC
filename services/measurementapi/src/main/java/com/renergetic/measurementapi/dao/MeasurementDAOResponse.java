4b7a912f903afa9033fa8496c12a8597e3bedf58
Raul Parra Ruiz <raul.parra.ext@inetum.com>
2023-09-21 13:22:39 +0200

    Added dev and prd properties and removed tag field

--- services/measurementapi/src/main/java/com/renergetic/measurementapi/dao/MeasurementDAOResponse.java
+++ services/measurementapi/src/main/java/com/renergetic/measurementapi/dao/MeasurementDAOResponse.java
@@ -3,6 +3,9 @@ package com.renergetic.measurementapi.dao;
 import java.util.HashMap;
 import java.util.Map;
 
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.annotation.JsonInclude.Include;
+
 import lombok.Getter;
 import lombok.Setter;
 import lombok.ToString;
@@ -13,6 +16,8 @@ import lombok.ToString;
 public class MeasurementDAOResponse {
 	String measurement;
 	Map<String, String> fields;
+
+	@JsonInclude(Include.NON_EMPTY)
 	Map<String, String> tags;
 
 	public MeasurementDAOResponse() {
