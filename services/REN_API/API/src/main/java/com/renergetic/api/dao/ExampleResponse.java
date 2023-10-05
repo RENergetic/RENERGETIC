9fa198ea4fb807f5efdc34d986a20fd178552539
Denigt <guerrero.raulparra@gmail.com>
2023-10-05 02:20:49 +0200

    Archetype project first version

---
+++ services/REN_API/API/src/main/java/com/renergetic/api/dao/ExampleResponse.java
@@ -0,0 +1,45 @@
+package com.renergetic.api.dao;
+
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.renergetic.api.model.ExampleEntity;
+
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+import java.time.ZonedDateTime;
+
+@Getter
+@Setter
+@RequiredArgsConstructor
+@ToString
+public class ExampleResponse {
+
+    @JsonFormat(pattern="yyyy-MM-dd HH:mm:ss")
+    @JsonProperty(required = true)
+    private ZonedDateTime requestDate;
+
+    @JsonProperty(required = true)
+    private Long id;
+	@JsonProperty(required = true)
+	private String name;
+	@JsonProperty(required = true)
+	private String data;
+
+    // Method to create this DTO from a JPA entity
+    public static ExampleResponse create(ExampleEntity example) {
+        ExampleResponse dao = new ExampleResponse();
+
+        dao.setRequestDate(ZonedDateTime.now());
+        dao.setId(example.getId());
+        dao.setName(example.getName());
+        dao.setData(example.getData());
+        
+        return dao;
+    }
+
+
+}
---
+++ services/REN_API/API/src/main/java/com/renergetic/api/exception/UnauthorizedAccessException.java
@@ -0,0 +1,25 @@
+package com.renergetic.api.exception;
+
+import org.springframework.http.HttpStatus;
+
+public class UnauthorizedAccessException extends HttpRuntimeException{
+
+	private static final long serialVersionUID = -3547042448040107613L;
+
+	public UnauthorizedAccessException() {
+		super();
+	}
+
+	public UnauthorizedAccessException(String formatString, Object... paramenters) {
+		super(formatString, paramenters);
+	}
+
+	public UnauthorizedAccessException(String message) {
+		super(message);
+	}
+	
+	@Override
+	public HttpStatus getHttpStatus() {
+		return HttpStatus.FORBIDDEN;
+	}
+}
