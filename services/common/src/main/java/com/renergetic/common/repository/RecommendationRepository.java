d560763ba204a6cc4b6c898cbd4f55e8cd2c778c
Tomasz Ciesielczyk <tomasz.ciesielczyk@gmail.com>
2023-11-07 05:27:13 +0100

    add missing getters & setters

--- services/common/src/main/java/com/renergetic/common/repository/RecommendationRepository.java
+++ services/common/src/main/java/com/renergetic/common/repository/RecommendationRepository.java
@@ -25,6 +25,7 @@ public interface RecommendationRepository extends JpaRepository<HDRRecommendatio
     @Query("SELECT hdr FROM HDRRecommendation hdr WHERE hdr.timestamp = :timestamp ")
     List<HDRRecommendation> findByTimestamp(LocalDateTime timestamp);
 
-    @Query("SELECT hdr FROM HDRRecommendation hdr WHERE hdr.timestamp = :timestamp and hdr.tag.key = :tagId ")
+    @Query("SELECT hdr FROM HDRRecommendation hdr WHERE hdr.timestamp = :timestamp and hdr.tag.id = :tagId ")
     List<HDRRecommendation> findByTimestampTag(LocalDateTime timestamp, Long tagId);
+
 }
