7ab8b84eb5fcd2f8c7e228337f724fdf6b2d7949
Tomasz Ciesielczyk <tomasz.ciesielczyk@gmail.com>
2024-03-09 12:26:55 +0100

    fix imports

--- services/common/src/main/java/com/renergetic/common/repository/MeasurementRepository.java
+++ services/common/src/main/java/com/renergetic/common/repository/MeasurementRepository.java
@@ -9,7 +9,7 @@ import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
-import jakarta.transaction.Transactional;
+import javax.transaction.Transactional;
 import java.util.List;
 
 @SuppressWarnings("unchecked")
--- services/common/src/main/java/com/renergetic/common/repository/MeasurementTagsRepository.java
+++ services/common/src/main/java/com/renergetic/common/repository/MeasurementTagsRepository.java
@@ -7,9 +7,9 @@ import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
+import javax.transaction.Transactional;
 import java.util.List;
 
-import jakarta.transaction.Transactional;
 
 @SuppressWarnings("unchecked")
 public interface MeasurementTagsRepository extends JpaRepository<MeasurementTags, Long> {
