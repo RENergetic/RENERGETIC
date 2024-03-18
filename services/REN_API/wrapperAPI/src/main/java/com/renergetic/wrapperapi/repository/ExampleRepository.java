fcd9c4f50077b97e20c81b1676e5b3f3e0e9c062
Raul Parra Ruiz <raul.parra.ext@inetum.com>
2024-03-18 07:25:02 +0100

    Delete example files in wrapper API

--- services/REN_API/wrapperAPI/src/main/java/com/renergetic/wrapperapi/repository/ExampleRepository.java
+++
@@ -1,9 +0,0 @@
-package com.renergetic.wrapperapi.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.renergetic.wrapperapi.model.ExampleEntity;
-
-public interface ExampleRepository extends JpaRepository<ExampleEntity, Long>{
-	
-}
