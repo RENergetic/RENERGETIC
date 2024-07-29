2b568e7384eefb83b09b2142422550dd81af09bf
tomasz_ciesielczyk <tomaszc@man.poznan.pl>
2024-07-29 18:29:38 +0200

    issues with meter name

--- services/kpiAPI/src/main/java/com/renergetic/kpiapi/dao/AbstractMeterTypeDAO.java
+++ services/kpiAPI/src/main/java/com/renergetic/kpiapi/dao/AbstractMeterTypeDAO.java
@@ -9,11 +9,13 @@ public class AbstractMeterTypeDAO {
 
     String meter;
     String description;
+    String name;
 
     @JsonProperty(value = "physical_name")
     String physicalName;
 
     public AbstractMeterTypeDAO(AbstractMeter meter) {
+        this.name = meter.name();
         this.meter = meter.meter;
         this.description = meter.description;
         this.physicalName = meter.physicalName;
