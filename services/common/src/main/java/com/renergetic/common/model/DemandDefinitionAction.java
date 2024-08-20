3c9b2793c34a1507205f01942867640d24ac57dc
Jeremy W. <jeremy.wautelet@inetum-realdolmen.world>
2024-08-20 08:31:57 +0200

    Fixing missing demand definition with frontend.

--- services/common/src/main/java/com/renergetic/common/model/DemandDefinitionAction.java
+++ services/common/src/main/java/com/renergetic/common/model/DemandDefinitionAction.java
@@ -3,6 +3,10 @@ package com.renergetic.common.model;
 public enum DemandDefinitionAction {
     INCREASE_TEMPERATURE,
     DECREASE_TEMPERATURE,
+    INCREASE_POWER,
+    DECREASE_POWER,
+    INCREASE_HEAT,
+    DECREASE_HEAT,
     CHARGE_TEMPERATURE,
     CHARGE_EV
 }
