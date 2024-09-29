ff8c6b948f3a43b2f29aeca2043d57d73893424f
tomasz_ciesielczyk <tomaszc@man.poznan.pl>
2024-09-29 18:56:04 +0200

    upate version

--- services/kpiAPI/src/main/java/com/renergetic/kpiapi/service/kpi/ESC.java
+++ services/kpiAPI/src/main/java/com/renergetic/kpiapi/service/kpi/ESC.java
@@ -39,7 +39,17 @@ public class ESC implements KPIFormula {
 
         Double result = (values.get(AbstractMeter.LOAD) + values.get(AbstractMeter.STORAGE) - values.get(AbstractMeter.ERS) - values.get(AbstractMeter.ENS)) /
                 (values.get(AbstractMeter.LNS) + values.get(AbstractMeter.LRS));
-
+//        from(bucket: "renergetic")
+//  |> range(start: v.timeRangeStart, stop: v.timeRangeStop)
+//  |> filter(fn: (r) => r["_measurement"] == "abstract_meter")
+//  |> filter(fn: (r) => r["measurement_type"] == "losses" or r["measurement_type"] == "ers" or r["measurement_type"] == "ens" or r["measurement_type"] == "lrs" or r["measurement_type"] == "lns" or r["measurement_type"] == "load")
+//  |> filter(fn: (r) => r["domain"] == "none")
+//  |> aggregateWindow(every: 12h, fn: sum, createEmpty: false)
+//   |> keep(columns: ["measurement_type", "_time", "_value"])
+// |> pivot(rowKey:["_time"], columnKey: ["measurement_type"], valueColumn: "_value")
+// |> map(fn: (r) => ({r with _value:( r["load"] - r["ers"]-r["ens"])/(r["lrs"]+r["lns"])*100.0}))
+//   |> keep(columns: [  "_time", "_value"])
+//  |> yield(name: "mean")
         if (!Double.isNaN(result) && !Double.isInfinite(result))
             return BigDecimal.valueOf(result);
         else return new BigDecimal(0);
