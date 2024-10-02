c4e0e102334f1fe2d30bf6148b237e4d574dafe6
Raul Parra Ruiz <raul.parra@inetum.com>
2024-10-02 09:05:18 +0200

    Add daos, integer and longs to double, raises error on string values

--- services/ingestionAPI/src/main/java/com/renergetic/ingestionapi/service/MeasurementService.java
+++ services/ingestionAPI/src/main/java/com/renergetic/ingestionapi/service/MeasurementService.java
@@ -7,7 +7,6 @@ import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
-import java.util.Optional;
 import java.util.Map.Entry;
 import java.util.stream.Collectors;
 
@@ -20,16 +19,15 @@ import com.influxdb.client.InfluxDBClient;
 import com.influxdb.client.WriteApi;
 import com.influxdb.client.domain.WritePrecision;
 import com.influxdb.client.write.Point;
-import com.renergetic.common.dao.FieldRestrictionsDAO;
+import com.renergetic.ingestionapi.dao.FieldRestrictionsDAO;
 import com.renergetic.common.dao.MeasurementIngestionDAO;
-import com.renergetic.common.dao.RestrictionsDAO;
+import com.renergetic.ingestionapi.dao.RestrictionsDAO;
 import com.renergetic.ingestionapi.exception.ConnectionException;
 import com.renergetic.common.model.Measurement;
 import com.renergetic.common.model.MeasurementType;
 import com.renergetic.common.model.PrimitiveType;
 import com.renergetic.common.model.Request;
 import com.renergetic.common.model.RequestError;
-import com.renergetic.common.model.details.MeasurementTags;
 import com.renergetic.common.repository.MeasurementRepository;
 import com.renergetic.common.repository.MeasurementTypeRepository;
 import com.renergetic.ingestionapi.service.utils.Restrictions;
@@ -51,7 +49,7 @@ public class MeasurementService {
 	@Autowired
 	private LogsService logs;
 	
-	private final int INFLUX_TRIES = 5;
+	private final static int INFLUX_TRIES = 5;
 
 	public Map<MeasurementIngestionDAO, Boolean> insert(List<MeasurementIngestionDAO> measurements, String bucket, RestrictionsDAO restrictions) {
 		int tries = 0;
@@ -87,14 +85,16 @@ public class MeasurementService {
 							if (!fieldKey.equalsIgnoreCase("time")) {
 								Entry<String, ?> field = Restrictions.parseField(fieldKey, fieldValue, restrictions);
 					    		
-								if (field.getValue() instanceof String)
-					    			registry.addField(field.getKey(), (String) field.getValue());
-					    		else if (field.getValue() instanceof Double)
-					    			registry.addField(field.getKey(), (Double) field.getValue());
-					    		else if (field.getValue() instanceof Long)
-					    			registry.addField(field.getKey(), (Long) field.getValue());
-					    		else if (field.getValue() instanceof Boolean)
-					    			registry.addField(field.getKey(), (Boolean) field.getValue());
+								if (field.getValue() instanceof Double)
+									registry.addField(field.getKey(), (Double) field.getValue());
+								else if (field.getValue() instanceof Long)
+									registry.addField(field.getKey(), (Long) field.getValue());
+								else if (field.getValue() instanceof Boolean)
+									registry.addField(field.getKey(), (Boolean) field.getValue());
+								else if (field.getValue() instanceof String) {
+									key.setErrorMessage("String values aren't allowed in field " + field.getKey());
+									checkedMeasurements.put(key, false);
+								}
 							}
 						});
 						
@@ -128,7 +128,7 @@ public class MeasurementService {
 		
 		Request requestInfo = logs.save(request);
 		ret.forEach((measurement, inserted) -> {
-			if (!inserted) {
+			if (Boolean.FALSE.equals(inserted)) {
 				logs.save(new RequestError("Object Not Inserted", measurement.getErrorMessage() != null? measurement.getErrorMessage() : "unknown", measurement.toString(), requestInfo));
 			}
 		});
@@ -138,33 +138,17 @@ public class MeasurementService {
 	
 	public List<String> getMeasurementNames(){
 		return repository.findByAssetIsNullAndAssetCategoryIsNull().stream().map(Measurement::getSensorName).collect(Collectors.toList());
-//				Arrays.asList(new String[]{"heat_pump", "energy_meter", "pv", "battery", "hot_water", "cold_water",
-//				"weather", "heat_exchange", "cooling_circuits", "chiller", "heat_meter", "dh_temperature",
-//				"dhw_temperature", "tapping_water", "thermostate", "temperature", "cpu", "renewability"});
 	}
 	
 	public List<FieldRestrictionsDAO> getFieldRestrictions(){
-		return typeRepository.findAll().stream().map(type -> new FieldRestrictionsDAO(type.getName(), PrimitiveType.DOUBLE, null)).collect(Collectors.toList());
-//			Arrays.asList(new FieldRestrictionsDAO[]{
-//			new FieldRestrictionsDAO("power", PrimitiveType.DOUBLE, null),
-//			new FieldRestrictionsDAO("energy", PrimitiveType.DOUBLE, null),
-//			new FieldRestrictionsDAO("percentage", PrimitiveType.INTEGER, null),
-//			new FieldRestrictionsDAO("co2_eq", PrimitiveType.DOUBLE, null),
-//			new FieldRestrictionsDAO("flow", PrimitiveType.DOUBLE, null),
-//			new FieldRestrictionsDAO("voltage", PrimitiveType.DOUBLE, null),
-//			new FieldRestrictionsDAO("temperature", PrimitiveType.DOUBLE, null),
-//			new FieldRestrictionsDAO("pressure", PrimitiveType.DOUBLE, null),
-//		});
+		return typeRepository
+			.findAll()
+			.stream()
+			.map(type -> new FieldRestrictionsDAO(type.getName(), PrimitiveType.DOUBLE, null, type.getUnit()))
+			.collect(Collectors.toList());
 	}
 	
 	public Map<String, String> getTagsRestrictions(){
-//		Map<String, String> tags = new HashMap<>();
-//		
-//		tags.put("asset_name", null);
-//		tags.put("prediction_window", "\\d+[Mdhms]");
-//		tags.put("predictive_model", null);
-//		tags.put("direction", "(?i)((in)|(out)|(none))");
-//		tags.put("domain", "(?i)((heat)|(electricity))");
 		List<Tags> tags = tagsRepository.findByMeasurementIdIsNull();
 
 		return tags.stream().collect(HashMap<String, String>::new, (m,v)->m.put(v.getKey(), v.getValue()), HashMap::putAll);
