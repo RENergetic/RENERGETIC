7bac2c678e1723463c477f742acea8b59d0e0575
tomasz_ciesielczyk <tomaszc@man.poznan.pl>
2024-10-17 07:40:30 +0200

    compile errors

--- services/hdrapi/src/main/java/com/renergetic/hdrapi/controller/DataController.java
+++ services/hdrapi/src/main/java/com/renergetic/hdrapi/controller/DataController.java
@@ -30,59 +30,59 @@ public class DataController {
     DataService dataSv;
 
     //=== GET REQUESTS ====================================================================================
-    @Operation(summary = "Get Data related with a panel id")
-    @ApiResponses({
-            @ApiResponse(responseCode = "200", description = "Request executed correctly"),
-            @ApiResponse(responseCode = "404", description = "No dashboards found related with this user")
-    })
-    @GetMapping(path = "/panel/{panelId}", produces = "application/json")
-    public ResponseEntity<DataWrapperDAO> getPanelData(
-            @PathVariable Long panelId,
-            @RequestParam("from") Optional<Long> from,
-            @RequestParam("to") Optional<Long> to) {
-        // GET INSTANT TO FIRST DAY OF THE CURRENT
+//    @Operation(summary = "Get Data related with a panel id")
+//    @ApiResponses({
+//            @ApiResponse(responseCode = "200", description = "Request executed correctly"),
+//            @ApiResponse(responseCode = "404", description = "No dashboards found related with this user")
+//    })
+//    @GetMapping(path = "/panel/{panelId}", produces = "application/json")
+//    public ResponseEntity<DataWrapperDAO> getPanelData(
+//            @PathVariable Long panelId,
+//            @RequestParam("from") Optional<Long> from,
+//            @RequestParam("to") Optional<Long> to) {
+//        // GET INSTANT TO FIRST DAY OF THE CURRENT
+//
+//        Long fromInstant = null;
+//
+//        if (from.isEmpty()) {
+//            Calendar calendar = Calendar.getInstance();
+//            calendar.set(Calendar.MILLISECOND, 0);
+//            calendar.set(Calendar.SECOND, 0);
+//            calendar.set(Calendar.MINUTE, 0);
+//            calendar.set(Calendar.HOUR_OF_DAY, 0);
+//            calendar.set(Calendar.DATE, 1);
+//            calendar.set(Calendar.MONTH, 0);
+//            fromInstant = calendar.toInstant().toEpochMilli();
+//        }
+//        return new ResponseEntity<>(dataSv.getPanelData(panelId, from.orElse(fromInstant), to),
+//                HttpStatus.OK);
+//    }
 
-        Long fromInstant = null;
-
-        if (from.isEmpty()) {
-            Calendar calendar = Calendar.getInstance();
-            calendar.set(Calendar.MILLISECOND, 0);
-            calendar.set(Calendar.SECOND, 0);
-            calendar.set(Calendar.MINUTE, 0);
-            calendar.set(Calendar.HOUR_OF_DAY, 0);
-            calendar.set(Calendar.DATE, 1);
-            calendar.set(Calendar.MONTH, 0);
-            fromInstant = calendar.toInstant().toEpochMilli();
-        }
-        return new ResponseEntity<>(dataSv.getPanelData(panelId, from.orElse(fromInstant), to),
-                HttpStatus.OK);
-    }
-
-    @Operation(summary = "Get Data related with a panel id and asset")
-    @ApiResponses({
-            @ApiResponse(responseCode = "200", description = "Request executed correctly"),
-            @ApiResponse(responseCode = "404", description = "No dashboards found related with this user")
-    })
-    @GetMapping(path = "/panel/{panelId}/asset/{assetId}", produces = "application/json")
-    public ResponseEntity<DataWrapperDAO> getAssetPanelData(
-            @PathVariable Long panelId,
-            @PathVariable Long assetId,
-            @RequestParam("from") Optional<Long> from,
-            @RequestParam("to") Optional<Long> to) {
-        DataWrapperDAO panelData =
-                dataSv.getPanelData(panelId, assetId, from.orElse((new Date()).getTime() - 3600000), to);
-        return new ResponseEntity<>(panelData, HttpStatus.OK);
-    }
-
-    @GetMapping(path = "/timeseries/tile/{tileId}", produces = "application/json")
-    public ResponseEntity<TimeseriesDAO> getTileTimeseries(
-            @PathVariable Long tileId,
-            @RequestParam("from") Optional<Long> from,
-            @RequestParam("to") Optional<Long> to) {
-        TimeseriesDAO tileTimeseries =
-                dataSv.getTileTimeseries(tileId, null, from.orElse((new Date()).getTime() - 3600000), to);
-        return new ResponseEntity<>(tileTimeseries, HttpStatus.OK);
-    }
+//    @Operation(summary = "Get Data related with a panel id and asset")
+//    @ApiResponses({
+//            @ApiResponse(responseCode = "200", description = "Request executed correctly"),
+//            @ApiResponse(responseCode = "404", description = "No dashboards found related with this user")
+//    })
+//    @GetMapping(path = "/panel/{panelId}/asset/{assetId}", produces = "application/json")
+//    public ResponseEntity<DataWrapperDAO> getAssetPanelData(
+//            @PathVariable Long panelId,
+//            @PathVariable Long assetId,
+//            @RequestParam("from") Optional<Long> from,
+//            @RequestParam("to") Optional<Long> to) {
+//        DataWrapperDAO panelData =
+//                dataSv.getPanelData(panelId, assetId, from.orElse((new Date()).getTime() - 3600000), to);
+//        return new ResponseEntity<>(panelData, HttpStatus.OK);
+//    }
+//
+//    @GetMapping(path = "/timeseries/tile/{tileId}", produces = "application/json")
+//    public ResponseEntity<TimeseriesDAO> getTileTimeseries(
+//            @PathVariable Long tileId,
+//            @RequestParam("from") Optional<Long> from,
+//            @RequestParam("to") Optional<Long> to) {
+//        TimeseriesDAO tileTimeseries =
+//                dataSv.getTileTimeseries(tileId, null, from.orElse((new Date()).getTime() - 3600000), to);
+//        return new ResponseEntity<>(tileTimeseries, HttpStatus.OK);
+//    }
 
     @GetMapping(path = "/timeseries/measurements/{measurements}", produces = "application/json")
     public ResponseEntity<TimeseriesDAO> getMeasurementsTimeseries(
@@ -96,16 +96,16 @@ public class DataController {
     }
 
 
-    @GetMapping(path = "/timeseries/tile/{tileId}/asset/{assetId}", produces = "application/json")
-    public ResponseEntity<TimeseriesDAO> getTileAssetTimeseries(
-            @PathVariable Long tileId,
-            @PathVariable Long assetId,
-            @RequestParam("from") Optional<Long> from,
-            @RequestParam("to") Optional<Long> to) {
-        TimeseriesDAO tileTimeseries =
-                dataSv.getTileTimeseries(tileId, assetId, from.orElse((new Date()).getTime() - 3600000), to);
-        return new ResponseEntity<>(tileTimeseries, HttpStatus.OK);
-    }
+//    @GetMapping(path = "/timeseries/tile/{tileId}/asset/{assetId}", produces = "application/json")
+//    public ResponseEntity<TimeseriesDAO> getTileAssetTimeseries(
+//            @PathVariable Long tileId,
+//            @PathVariable Long assetId,
+//            @RequestParam("from") Optional<Long> from,
+//            @RequestParam("to") Optional<Long> to) {
+//        TimeseriesDAO tileTimeseries =
+//                dataSv.getTileTimeseries(tileId, assetId, from.orElse((new Date()).getTime() - 3600000), to);
+//        return new ResponseEntity<>(tileTimeseries, HttpStatus.OK);
+//    }
     //TODO:
 //    @GetMapping(path = "/timeseries/panel/{panelId}", produces = "application/json")
 //    public ResponseEntity<TimeseriesDAO> getTileTimeseries(
