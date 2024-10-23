40e5de72d223e9aaad94be5c6e37d84e87cda704
tomasz_ciesielczyk <tomaszc@man.poznan.pl>
2024-10-23 13:09:00 +0200

    add comment & validation

--- services/common/src/main/java/com/renergetic/common/dao/MeasurementTypeDAORequest.java
+++ services/common/src/main/java/com/renergetic/common/dao/MeasurementTypeDAORequest.java
@@ -1,6 +1,7 @@
 package com.renergetic.common.dao;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
+import com.renergetic.common.exception.InvalidArgumentException;
 import com.renergetic.common.model.MeasurementType;
 import lombok.*;
 
@@ -10,7 +11,11 @@ import lombok.*;
 @AllArgsConstructor
 @ToString
 public class MeasurementTypeDAORequest {
-    //TODO: store symbol ,"ºC",
+
+    /**
+     * ID is optional when object is used tp infer measurement type (for example during import/export of dashboards, where id's might be different for pilotss) -
+     * the id of the measurement type (or list of  measurement type ids ) is inferred from other fields
+     */
     @JsonProperty(required = false)
     private Long id;
     @JsonProperty(required = false, value = "name")
@@ -28,6 +33,11 @@ public class MeasurementTypeDAORequest {
     }
 
     public MeasurementType mapToEntity() {
+        if (this.getId() == null) {
+            if (this.name == null && this.physicalName == null && this.unit == null) {
+                throw new InvalidArgumentException("Missing at least one non nullable field");
+            }
+        }
         var mt = new MeasurementType();
         mt.setPhysicalName(this.getPhysicalName());
         mt.setId(this.getId());
